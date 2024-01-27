aaedacbc334aa673f44273ca00d4898d1da78a94
KinshasaPolee <kpolee1@gmail.com>
2024-01-27 17:25:50 -0500

    updated files

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,15 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
-    })
-);
+app.use(cors());
+
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type"],
+//     })
+// );
 
 app.get("/", (request, response) => {
     console.log(request)
