51bc59cbc0d9310de0c7e7e9450a345879a8e553
KinshasaPolee <kpolee1@gmail.com>
2024-01-30 10:12:18 -0500

    added table data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,6 +31,43 @@ const Home = () => {
                 <MdOutlineAddBox className='text-sky-800 text-4xl' />
                 </Link>
             </div>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <table className='w-full border-separate border-spacing-2'>
+                    <thead>
+                        <tr>
+                            <th className='border border-slate-600 rounded-md'>No</th>
+                            <th className='border border-slate-600 rounded-md'>Title</th>
+                            <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+                            <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+                            <th className='border border-slate-600 rounded-md'>Operations</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {books.map((book, index) => (
+                            <tr key={book.id} className='h-8'>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                {index + 1}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                {book.title}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                                {book.author}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                                {book.publishYear}
+                                </td>
+                                <td className='border border-slate-700 rounded-md text-center'>
+                                <div></div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
